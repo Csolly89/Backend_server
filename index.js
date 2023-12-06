fd3ae86462397034f032a53470533fa08008d1bf
Csolly89 <141281525+Csolly89@users.noreply.github.com>
2023-12-06 18:24:08 -0500

    base setup made..db connected

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+// database connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('db connected'))
+.catch(err => console.log(err))
+
+const PORT = process.env.PORT || 8080
+app.listen(PORT, console.log(`listening on port ${PORT}`))
